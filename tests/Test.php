8264be23cc86229a3922f70b590c8d2aea2138b3
Federico Seckel <fseckel@dc.uba.ar>
2015-05-07 13:27:20 -0300

    Test

--- tests/Test.php
+++ tests/Test.php
@@ -1,13 +1,36 @@
 <?php
-class TestOfUsuario extends UnitTestCase {
-	function __construct() {
-		$esUnaPruebaEntoncesIgnorarSesiones = TRUE;
+
+class TestOfUsuario extends UnitTestCase{
+
+    function __construct(){
+        $esUnaPruebaEntoncesIgnorarSesiones = TRUE;
         chdir(dirname(dirname(__FILE__)));
-		include 'prepend.php';
+        include 'prepend.php';
     }
-	
-    function testTrue() {
-		$this->assertTrue(TRUE);
-	}
+
+    function test_constructFromData(){
+        $datos   = array(
+            'id'           => 9,
+            'user'         => 'fsec',
+            'pass'         => 'contra',
+            'ultimoLogin'  => '2015-05-01 03:19:59',
+            'loginEnabled' => 1,
+            'verificacion' => 'veri',
+            'mail'         => 'mail@server.com',
+            'nombre'       => 'este es mi nombre',
+            'orden'        => '3'
+        );
+        $usuario = new usuario(NULL, USER_MANUAL_DEFINE, $datos);
+        
+        $this->assertEqual($usuario->getId(), $datos['id']);
+        $this->assertEqual($usuario->getUser(), $datos['user']);
+        $this->assertEqual($usuario->getPass(), $datos['pass']);
+        $this->assertEqual($usuario->getUltimoLogin(), $datos['ultimoLogin']);
+        $this->assertEqual($usuario->getLoginEnabled(), $datos['loginEnabled']);
+        $this->assertEqual($usuario->getVerificacion(), $datos['verificacion']);
+        $this->assertEqual($usuario->getMail(), $datos['mail']);
+        $this->assertEqual($usuario->getNombre(), $datos['nombre']);
+        $this->assertEqual($usuario->getOrden(), $datos['orden']);
+    }
 
 }
