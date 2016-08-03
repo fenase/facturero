9379db9a56b66e5337777ad3b5f85ee88169a102
Federico Seckel <fseckel@dc.uba.ar>
2016-08-03 20:57:46 -0300

    Cambios en la forma de manejar conexiones db

--- private/funcionesDB.php
+++ private/funcionesDB.php
@@ -1,47 +1,63 @@
 <?php
+/**
+ * Controlador de base de datos
+ * @package backEnd
+ */
 
+/**
+ * controla conexiones hacia la base de datos. Puede manejar varias conexiones al mismo tiempo (distintos usuarios, schemas, etc.)
+ * @package backEnd/database
+ */
 class Database{
 
+    /** @var int almacena el último id de un insert sobre PK con autoincrement */
     var $insert_id;
+    /** @var array todas las conexiones abiertas en dicionario sobre los datos que las distinghen */
     private static $links;
+    /** @var string clave dentro del diccionario $links de la conexión referenciada por esta instabcia de Database */
     private $thisLink;
+    /** @var string clave dentro del diccionario $links de la conexión referenciada por esta instabcia de Database CON PASSWORD CENSURADA LARA LOG */
+    private $thisLinkREDACTED;
+    /** @var int Cuenta la cantidad de instancias abiertas existen para cada conexión. Util para el destructor */
     private static $references;
 
     /**
      * Constructor. Abre una conexión MySQLi
-     * @param type $host
-     * @param type $user
-     * @param type $pass
-     * @param type $db
+     * @param string $host
+     * @param string $user
+     * @param string $pass
+     * @param string $db
      */
     function __construct($host = DBHOST, $user = DBUSER, $pass = DBPASS, $db = DBDB){
         $this->thisLink = "h:$host::u:$user::p:$pass::d:$db";
+        $this->thisLinkREDACTED = "h:$host::u:$user::p:******::d:$db";
         if(!self::$links[$this->thisLink]){
             self::$links[$this->thisLink] = new mysqli($host, $user, $pass, $db);
             if(mysqli_connect_error()){
                 throw new Exception('imposible conectar a la base de datos: ' . mysqli_connect_error());
             }else{
-                self::$references++;
+                self::$references[$this->thisLink]++;
                 $logger = new Logger();
-                $logger->log("Conectado a la base de datos $db en $host (id: ".self::$links[$this->thisLink]->thread_id.") (Refs=".self::$references.")");               
+                $logger->log("Conectado a la base de datos $db en $host (id: ".self::$links[$this->thisLink]->thread_id.") (Refs[".$this->thisLinkREDACTED."]=".self::$references[$this->thisLink].")");               
             }
         }else{
-            self::$references++;
+            self::$references[$this->thisLink]++;
             $logger = new Logger();
-            $logger->log("Base de datos $db en $host ya conectada. Reutilizando vínculo (Refs=".self::$references.")");
+            $logger->log("Base de datos $db en $host ya conectada. Reutilizando vínculo (Refs[".$this->thisLinkREDACTED."]=".self::$references[$this->thisLink].")");
         }
     }
 
     /**
      * Se invoca cuando se pierden todas las referencias al objeto (o al final del script).
+     * Cierra las conexiones que pierden referencia
      */
     function __destruct(){
-        self::$references--;
-        if(!self::$references){
+        self::$references[$this->thisLink]--;
+        if(!self::$references[$this->thisLink]){
             $this->cerrar();
         }else{
             $logger = new Logger;        
-            $logger->log("No se cierra base de datos ya que quedan referencias (Refs=".self::$references.")");
+            $logger->log("No se cierra base de datos (id ".self::$links[$this->thisLink]->thread_id.") ya que quedan referencias (Refs[".$this->thisLinkREDACTED."]=".self::$references[$this->thisLink].")");
         }
     }
 
@@ -82,7 +98,7 @@ class Database{
 
     /**
      * Escapa las cadenas para poder insertarlas en la db.
-     * @param type $string
+     * @param string $string
      * @return type
      */
     function escape_string($string){
